10ff5ce3739eeded0bdff6d591fca4e3fea08e0f
Rick Neeft <rick@chroomsoft.nl>
2020-01-17 20:43:31 +0100

    Start

---
+++ XamarinDotNetCoreIssue/XamarinDotNetCoreIssue/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinDotNetCoreIssue
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = Startup.ServiceProvider.GetService<MyViewModel>();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ((MyViewModel)BindingContext).DoIt();
+        }
+    }
+}
---
+++ XamarinDotNetCoreIssue/XamarinDotNetCoreIssue/Startup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Xamarin.Essentials;
+
+namespace XamarinDotNetCoreIssue
+{
+    public static class Startup
+    {
+        public static IServiceProvider ServiceProvider { get; set; }
+        public static void Init()
+        {
+            var a = Assembly.GetExecutingAssembly();
+            using var stream = a.GetManifestResourceStream("XamarinDotNetCoreIssue.appsettings.json");
+
+            var host = new HostBuilder()
+                        .ConfigureHostConfiguration(c =>
+                        {
+                // Tell the host configuration where to file the file (this is required for Xamarin apps)
+                c.AddCommandLine(new string[] { $"ContentRoot={FileSystem.AppDataDirectory}" });
+
+                //read in the configuration file!
+                c.AddJsonStream(stream);
+                        })
+                        .ConfigureServices((c, x) =>
+                        {
+                // Configure our local services and access the host configuration
+                ConfigureServices(c, x);
+                        })
+                        .ConfigureLogging(l => l.AddConsole(o =>
+                        {
+                            //setup a console logger and disable colors since they don't have any colors in VS
+                            o.DisableColors = true;
+                        }))
+                        .Build();
+
+            //Save our service provider so we can use it later.
+            ServiceProvider = host.Services;
+
+        }
+
+        static void ConfigureServices(HostBuilderContext ctx, IServiceCollection services)
+        {
+            // The HostingEnvironment comes from the appsetting.json and could be optionally used to configure the mock service
+            if (ctx.HostingEnvironment.IsDevelopment())
+            {
+                // add as a singleton so only one ever will be created.
+                services.AddSingleton<IDataService, MockDataService>();
+            }
+            else
+            {
+                services.AddSingleton<IDataService, MyDataService>();
+            }
+
+            // add the ViewModel, but as a Transient, which means it will create a new one each time.
+            services.AddTransient<MyViewModel>();
+
+            //Another thing we can do is access variables from that json file
+            var world = ctx.Configuration["Hello"];
+        }
+    }
+}
